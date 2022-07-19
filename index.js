34ace802809ed29bb513eb8b0fe4c32a611fbb71
José Luis González Santana <yunior@squaads.com>
2022-07-19 18:24:42 +0100

    Cambios en release

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ dotenv.config()
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send(`Estoy ahora mismo en la rama MAIN y tengo esta variable de entorno EOI ${process.env.EOI}`);
+    res.send(`Estoy ahora mismo en la rama RELEASE y tengo esta variable de entorno EOI ${process.env.EOI}`);
 });
 
 app.listen(3000, () => {
